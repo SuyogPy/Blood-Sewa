5245d33bc3daf5bcd841f84d235e5b2bd65b1ea6
Prashant <prashant432d3dd900@gmail.com>
2026-03-28 17:05:29 +0545

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,4 @@
 <?php
-// =============================================
-// db.php - Database Connection File
-// =============================================
-// This file connects to the MySQL database
-// using mysqli (MySQL Improved Extension).
-// We include this file in other PHP files
-// whenever we need to access the database.
-// =============================================
 
 // Database connection details (for XAMPP)
 $server = "localhost";     // Server name (localhost for XAMPP)
@@ -14,14 +6,13 @@ $username = "root";        // Default XAMPP username
 $password = "";            // Default XAMPP password (empty)
 $database = "blood_sewa";  // Our database name
 
-// Create connection using mysqli
+
 $conn = new mysqli($server, $username, $password, $database);
 
-// Check if connection was successful
+
 if ($conn->connect_error) {
-    // If connection fails, show error and stop
+    
     die("Connection failed: " . $conn->connect_error);
 }
 
-// If we reach here, connection is successful
 ?>
